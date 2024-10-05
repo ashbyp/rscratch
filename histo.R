829e1bea6a33424911dcdfe8bfc438a30c289ae6
ashbyp <ashbyp@yahoo.co.uk>
2024-10-06 00:07:52 +0100

    tired of driving

--- histo.R
+++ histo.R
@@ -6,3 +6,30 @@ hist(iris$Sepal.Length)
 hist(iris$Sepal.Width)
 hist(iris$Petal.Length)
 hist(iris$Petal.Width)
+
+par(mfrow = c(3, 1))
+
+hist(iris$Petal.Width [iris$Species == "setosa"],
+  xlim = c(0, 3),
+  breaks = 9,
+  main = "Petal Width for Setosa",
+  xlab = "",
+  col = "red")
+
+hist(iris$Petal.Width [iris$Species == "versicolor"],
+     xlim = c(0, 3),
+     breaks = 9,
+     main = "Petal Width for Virsicolor",
+     xlab = "",
+     col = "yellow")
+
+hist(iris$Petal.Width [iris$Species == "virginica"],
+     xlim = c(0, 3),
+     breaks = 9,
+     main = "Petal Width for Virginca",
+     xlab = "",
+     col = "blue")
+
+par(mfrow = c(1, 1))
+
+detach("package:datasets", unload = TRUE)
